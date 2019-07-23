cecc3ac213a1f598cb03d85f3d416d5e95cc0059
Rizqi Nur Assyaufi <bandithijo@gmail.com>
2019-07-24 00:36:51 +0800

    12. Create add a new book (:new & :create)

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,4 +6,24 @@ class BooksController < ApplicationController
   def show
     @book = Book.find(params[:id])
   end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(resource_params)
+    if @book.save
+      flash[:notice] = 'Buku berhasil diinputkan ke dalma database!'
+      redirect_to books_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def resource_params
+    params.require(:book).permit(:title, :description, :page, :price, :author_id)
+  end
 end

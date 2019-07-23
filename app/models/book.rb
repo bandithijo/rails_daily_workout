cfb49fb6894881e54ae4c2937ba6021c218d29de
Rizqi Nur Assyaufi <bandithijo@gmail.com>
2019-07-24 01:11:36 +0800

    15. Validation for form new book + message

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,9 @@
 class Book < ApplicationRecord
   belongs_to :author
+
+  validates :title, presence: {message: 'Judul tidak boleh kosong!'}
+  validates :description, length: {minimum: 10, message: 'Minimal 10 karakter!'}
+  validates :author_id, presence: {message: 'Penulis belum dipilih!'}
+  validates :page, numericality: {greather_than_or_equal_to: 1, message: 'Halaman harus lebih dari 0!'}
+  validates :price, numericality: {minimum: 0, message: 'Harga minimal 0!'}
 end

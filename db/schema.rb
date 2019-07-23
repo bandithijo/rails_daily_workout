b6ccc037bd4e37b23cb90ff8ff286aa510bd7cbf
Rizqi Nur Assyaufi <bandithijo@gmail.com>
2019-07-24 07:48:21 +0800

    20. Create tabel users [name, username, timestamps]

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_23_140619) do
+ActiveRecord::Schema.define(version: 2019_07_23_234729) do
 
   create_table "authors", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", limit: 100
@@ -28,6 +28,13 @@ ActiveRecord::Schema.define(version: 2019_07_23_140619) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "author_id"
+  end
+
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
+    t.string "username"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end

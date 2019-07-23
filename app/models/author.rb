2032c060c45cb29f9787b1a56777c830342672fb
Rizqi Nur Assyaufi <bandithijo@gmail.com>
2019-07-24 07:32:27 +0800

    14. Validation on model author + message at bottom

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,3 +1,7 @@
 class Author < ApplicationRecord
   has_many :books
+
+  validates :name, presence: {message: 'Nama harus diisi!'}
+  validates :address, length: {minimum: 4, message: 'Alamat, minimal 4 karakter!'}
+  validates :old, numericality: {greather_than: 0, message: 'Umur harus diisi!'}
 end

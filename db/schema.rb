d538c3b1656135855d4e3ca4960e96bc8dcc941e
Rizqi Nur Assyaufi <bandithijo@gmail.com>
2019-07-24 07:49:47 +0800

    21. Add column password digest to table users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_23_234729) do
+ActiveRecord::Schema.define(version: 2019_07_23_234914) do
 
   create_table "authors", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", limit: 100
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 2019_07_23_234729) do
     t.string "username"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end

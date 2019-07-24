82c63240c5ad55ce2b73eb73ed68574840297d11
Rizqi Nur Assyaufi <bandithijo@gmail.com>
2019-07-24 09:03:38 +0800

    26. Create validates for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
+
+  validates :name, presence: {message: 'Nama harus diisi!'}
+  validates :username, uniqueness: {message: 'Username sudah pernah didaftarkan'}
+  validates :password, presence: {message: 'Password tidak boleh kosong!'}, length: {minimum: 8, message: 'Password minimal 8 karakter!'}
 end

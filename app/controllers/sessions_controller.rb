79a0a563c62477aed9e4ed3f1376b72408813167
Rizqi Nur Assyaufi <bandithijo@gmail.com>
2019-07-24 12:23:21 +0800

    26. Create sessions controller for user login

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    username = params[:username]
+    password = params[:password]
+
+    user = User.find_by(username: username)
+
+    if user
+      if user.authenticate(password)
+        redirect_to root_path, notice: "Anda berhasil login sebagai, #{user.name}"
+      else
+        redirect_to signin_path, notice: 'Username atau password yang anda masukkan, salah!'
+      end
+    else
+      redirect_to signin_path, notice: 'Username berlum terdaftar!'
+    end
+  end
+end
